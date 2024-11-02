b0169e71e479fcf21612e2a84997dc77c9913ba0
Yves-M22 <sebastianYSM12@gmail.com>
2024-11-01 22:19:45 -0700

    setup

--- client/src/components/Expense/ExpenseList.tsx
+++ client/src/components/Expense/ExpenseList.tsx
@@ -1,15 +1,31 @@
 import ExpenseItem from "./ExpenseItem";
 import { AppContext } from "../../context/AppContext";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { Expense } from "../../types/types";
+import { fetchExpenses } from "../../utils/expense-utils";
 
 const ExpenseList = () => {
-  const { expenses } = useContext(AppContext);
+  const expenses = useContext(AppContext);
+
+  // Fetch expenses on component mount
+  useEffect(() => {
+	loadExpenses();
+  }, []);
+
+  // Function to load expenses and handle errors
+  const loadExpenses = async () => {
+	try {
+  	const expenseList = await fetchExpenses();
+  	expenses.setExpenses(expenseList);
+	} catch (err: any) {
+  	console.log(err.message);
+	}
+  };
 
   return (
     <ul data-testId="list" className="list-group">
-      {expenses.map((expense: Expense) => (
-        <ExpenseItem id={expense.id} name={expense.name} cost={expense.cost} />
+      {expenses.expenses.map((expense: Expense) => (
+        <ExpenseItem id={expense.id} description={expense.description} cost={expense.cost} />
       ))}
     </ul>
   );
