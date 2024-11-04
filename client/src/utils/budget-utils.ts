746a9b4ee5551094417a2e41b61b6cebb6564c13
earanda4 <earanda@ucsd.edu>
2024-11-03 22:06:30 -0800

    Create budget-utils.ts
    task 2

---
+++ client/src/utils/budget-utils.ts
@@ -0,0 +1,17 @@
+import { API_BASE_URL } from "../constants/constants";
+
+// Function to get budget from the backend. Method: GET
+export const fetchBudget = async (): Promise<number> => {
+  const response = await fetch(`${API_BASE_URL}/budget`, {
+    method: "GET",
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch budget");
+  }
+
+  const data = await response.json();
+  console.log("Fetched budget data:", data);
+
+  return data.data; // Assuming the budget value is in `data.data`
+};
