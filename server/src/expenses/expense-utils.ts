4fdb59d24599f8f82c5f4e5278b4bde8c6e22300
Yves-M22 <sebastianYSM12@gmail.com>
2024-11-10 01:28:18 -0800

    finished

--- server/src/expenses/expense-utils.ts
+++ server/src/expenses/expense-utils.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { Database } from "sqlite";
+import { Expense } from "../types";
 
 export async function createExpenseServer(req: Request, res: Response, db: Database) {
 
@@ -23,21 +24,33 @@ export async function createExpenseServer(req: Request, res: Response, db: Datab
  
 
 export function deleteExpense(req: Request, res: Response, db: Database) {
-    // TO DO: Implement deleteExpense function
-    const { id, cost, description } = req.params;
-    if(!id){
-        return res.status(400).send({error: "Missing required parameter: id"});
-    }
-    const index = expenses.findIndex(expenses => expenses.id === id);
-    console.log(id);
-    if(index === -1){
-        return res.status(404).send({ error: "Expense not found" });
-    }
-    expenses.splice(index, 1);
-    res.status(204).send();
+    try {
+        // Type casting the request body to the expected format.
+        const { id, cost, description } = req.params
+ 
+        if(!id){
+            return res.status(400).send({error: "Missing required parameter: id"});
+        }
+
+        if((db.get('SELECT id FROM expenses WHERE id=?;', id)) === null) {
+            return res.status(404).send({ error: "Expense not found" });
+        }
+    
+        db.run('DELETE FROM expenses WHERE id=?;', id);
+        res.status(204).send();
+
+    } catch (error) {
+        return res.status(400).send({ error: `Expense could not be deleted, + ${error}` });
+    };
+
+
 
 }
 
-export function getExpenses(req: Request, res: Response, db: Database) {
-    res.status(200).send({ "data": expenses });
+export async function getExpenses(req: Request, res: Response, db: Database) {
+
+    const data = await db.all('SELECT * FROM expenses');
+    res.status(200).send({ "data": data });
+        
+
 }
